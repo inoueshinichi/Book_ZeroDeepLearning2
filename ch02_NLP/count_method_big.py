c9416673643d062bcaf7038b1a620f8d1f6f734e
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2021-07-05 17:53:27 +0900

    update

---
+++ ch02_NLP/count_method_big.py
@@ -0,0 +1,46 @@
+import sys
+sys.path.append("/Users/inoueshinichi/Desktop/DeepLearning2_NLP") # 親ディレクトリのファイルをインポートするための設定
+sys.path.append("/home/inoue/Desktop/DeepLearning2_NLP")
+from common.utils import np, preprocess, create_co_matrix, cos_similarity, most_similar, ppmi
+
+from dataset import ptb
+
+import matplotlib.pyplot as plt
+
+
+def test_count_method_big():
+    """PTBコーパスデータセットに対して、
+    共起行列、正の相互情報量行列、SVDによる次元削減を行う.
+
+    SVDによる次元削減はO(N^3)なので、高速なscikit-learn版を使う.
+    """
+    window_size = 2
+    wordvec_size = 100
+
+    corpus, word_to_id, id_to_word = ptb.load_data('train')
+    vocab_size = len(word_to_id)
+    print('counting co-occurence...')
+    C = create_co_matrix(corpus, vocab_size, window_size)
+    print('calculating PPMI...')
+    W = ppmi(C, verbose=True)
+
+    print('calcurating SVD ...')
+    try:
+        # truncated SVD (fast!)
+        from sklearn.utils.extmath import randomized_svd
+        U, S, V = randomized_svd(W, n_components=wordvec_size, n_iter=5, random_state=None)
+    except ImportError:
+        # SVD (slow)
+        U, S, V = np.linalg.svd(W)
+    
+    word_vecs = U[:, :wordvec_size]
+
+    querys = ['you', 'year' 'car', 'toyota']
+    for query in querys:
+        most_similar(query, word_to_id, id_to_word, word_vecs, top=5)
+    
+
+    
+if __name__ == "__main__":
+    test_count_method_big()
+
---
+++ ch02_NLP/show_ptb.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append("/Users/inoueshinichi/Desktop/DeepLearning2_NLP") # 親ディレクトリのファイルをインポートするための設定
+sys.path.append("/home/inoue/Desktop/DeepLearning2_NLP")
+
+from dataset import ptb
+
+
+def test_show_ptb():
+    """Penn Treebank(ペン・ツリー・バンク)の一部を表示する.
+    手頃なサイズのコーパス
+    Word2Vecの作者(Tomas Mikolov)のWebページからダウンロードできる.
+    """
+
+    corpus, word_to_id, id_to_word = ptb.load_data('train')
+
+    print('corpus size: ', len(corpus))
+    print('corpus[:30]: ', corpus[:30])
+    print()
+    print('id_to_word[0]: ', id_to_word[0])
+    print('id_to_word[1]: ', id_to_word[1])
+    print('id_to_word[2]: ', id_to_word[2])
+    print()
+    print("word_to_id['car']: ", word_to_id['car'])
+    print("word_to_id['happy']: ", word_to_id['happy'])
+    print("word_to_id['lexus']: ", word_to_id['lexus'])
+
+
+if __name__ == "__main__":
+    test_show_ptb()
+
+
+
